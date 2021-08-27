2deaf305d46db52b0ed72c57b3bc0e423c575bc9
bohdan.leonets <HfewHT5aEu>
2021-08-27 18:26:51 +0300

    vue was created

--- client/vue.js
+++ client/vue.js
@@ -1,1 +1,23 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js';
+
+new Vue({
+    el: '#app',
+    data() {
+        return{
+            form: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                todo: ''
+            }
+        }
+    },
+    methods: {
+        createTodo() {
+            const {...todo} = this.form;
+            console.log(todo);
+
+            this.form.firstName = this.form.lastName = this.form.email = this.form.todo = '';
+        }
+    }
+})
