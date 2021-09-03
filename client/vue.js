cdb5d65a2c0bce12caec3b6b14b0945f3a0c7e5c
bohdan.leonets <bohdan.leonets@ria.com>
2021-09-03 16:30:25 +0300

    methods for buttons were added

--- client/vue.js
+++ client/vue.js
@@ -13,13 +13,28 @@ new Vue({
             todos: []
         }
     },
+    computed: {
+        canCreate() {
+            return this.form.firstName.trim() 
+                && this.form.lastName.trim() 
+                && this.form.email.trim() 
+                && this.form.todo.trim();
+        }
+    },
     methods: {
         createTodo() {
             const {...todo} = this.form;
 
-            this.todos.push({...todo, id: Date.now()});
+            this.todos.push({...todo, id: Date.now(), marked: false});
 
             this.form.firstName = this.form.lastName = this.form.email = this.form.todo = '';
+        },
+        markTodo(id) {
+            const todo = this.todos.find(td => td.id === id);
+            todo.marked = true;
+        },
+        deleteTodo(id) {
+            this.todos = this.todos.filter(td => td.id !== id);
         }
     }
 })
